6db5f786027c9273b5491ff79834bcbb25c3742d
prince <princeakpabio90@gmail.com>
2020-08-21 15:50:11 +0100

    added cart-icon and cart-dropdown components to app, also implemented cartReducer

---
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { toggleCartHidden } from '../../redux/cart/cart.action';
+import { connect } from 'react-redux';
+
+import { ReactComponent as ShoppingIcon } from '../../Assets/11.2 shopping-bag.svg.svg';
+
+import './cart-icon.styles.scss';
+
+
+const CartIcon = ({toggleCartHidden}) => (
+ <div className='cart-icon' onClick={toggleCartHidden}>
+   <ShoppingIcon className='shopping-icon' />
+   <span className='item-count'>0</span>
+ </div>
+)
+
+// const mapStateToProps = state => ({
+
+// })
+
+const mapDispatchToProps = dispatch => ({
+ toggleCartHidden: () => dispatch(toggleCartHidden())
+})
+
+export default connect(null, mapDispatchToProps)(CartIcon);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,10 +4,12 @@ import {ReactComponent as Logo} from '../../Assets/crown.svg'
 import './header.styles.scss';
 import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropDown from '../cart-dropdown/cart-dropdown.component';
 
 
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -29,13 +31,19 @@ const Header = ({currentUser}) => (
         <Link className="option" to="/signin">
           SIGN IN
         </Link>
-      )}
+        )}
+      <CartIcon />
     </div>
+    {
+      hidden ? null : <CartDropDown />
+    }
   </div>
 );
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+  currentUser,
+  hidden
+
 })
 
 export default connect(mapStateToProps)(Header);
